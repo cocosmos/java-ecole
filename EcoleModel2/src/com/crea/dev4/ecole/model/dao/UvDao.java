0e2df5a7e15022bbf5b9aa0c1f3d9dc64409f3e1
Cocosmos <mipam.guillot@crea-inseec.com>
2022-06-23 17:21:44 +0200

    finish unit test UV

--- EcoleModel2/src/com/crea/dev4/ecole/model/dao/UvDao.java
+++ EcoleModel2/src/com/crea/dev4/ecole/model/dao/UvDao.java
@@ -8,19 +8,24 @@ import com.crea.dev4.ecole.model.beans.Uv;
 import com.crea.dev4.ecole.model.utils.DBAction;
 
 public class UvDao {
-	/* GET Functions */
-	// get uv by code
+
+	/**
+	 * Get Uv by code
+	 * 
+	 * @param code of uv
+	 * @return the UV
+	 */
 	public static Uv getUvByCode(String code) {
 		Uv u = new Uv();
 		String request = null;
 		ResultSet response = null;
-		// =1=> connexion è la BD
+		// =1=> connexion a la BD
 		DBAction.DBConnexion();
-		// =2.1=> prÃ©parer notre requète sql SELECT
+		// =2.1=> prÃ©parer notre requete sql SELECT
 		request = "SELECT * FROM uv WHERE code = \'" + code + "\'";
 		try {
 			response = DBAction.getStm().executeQuery(request);
-			// 3==> La rècupèration du rèsultat dans un objet UV
+			// 3==> La recuperation du resultat dans un objet UV
 			if (response.next()) {
 				u.setCode(response.getString(1));
 				u.setNbh(response.getInt(2));
@@ -35,14 +40,17 @@ public class UvDao {
 		return u;
 	}
 
-	// get all UV
+	/**
+	 * Get all UV
+	 * 
+	 * @return an arraylist of all UV
+	 */
 	public static ArrayList<Uv> getAllUvs() {
 		ArrayList<Uv> listeUvs = new ArrayList<Uv>();
 		String request = null;
 		ResultSet response = null;
-		int i = 0;
 
-		// =1=> connexion è la BD
+		// =1=> connexion ï¿½ la BD
 		DBAction.DBConnexion();
 		request = "SELECT * FROM uv";
 		try {
@@ -54,7 +62,7 @@ public class UvDao {
 				uvtmp.setCoord(response.getString(3));
 				uvtmp.affiche();
 				listeUvs.add(uvtmp);
-				i++;
+
 			}
 			if (!response.isClosed()) {
 				response.close();
@@ -66,14 +74,18 @@ public class UvDao {
 		return listeUvs;
 	}
 
-	// get all uvs with number of hours
+	/**
+	 * Get UV by number of hours superior
+	 * 
+	 * @param nbh you want to test
+	 * @return an arraylist of Uv
+	 */
 	public static ArrayList<Uv> getUvsWithHours(int nbh) {
 		ArrayList<Uv> listeUvs = new ArrayList<Uv>();
 		String request = null;
 		ResultSet response = null;
-		int i = 0;
 
-		// =1=> connexion è la BD
+		// =1=> connexion a la BD
 		DBAction.DBConnexion();
 		request = "SELECT * FROM uv WHERE nbh > " + nbh;
 		try {
@@ -85,7 +97,7 @@ public class UvDao {
 				uvtmp.setCoord(response.getString(3));
 				uvtmp.affiche();
 				listeUvs.add(uvtmp);
-				i++;
+
 			}
 			if (!response.isClosed()) {
 				response.close();
@@ -97,56 +109,90 @@ public class UvDao {
 		return listeUvs;
 	}
 
-	/* ADD Functions */
-	/*
-	 * public static int addUv(Uv newUv) { int result = -1; String request = null;
-	 * 
-	 * DBAction.DBConnexion(); request = "INSERT INTO uv (code, nbh, coord)" +
-	 * " VALUES ('" + newUv.getCode() + "'," + newUv.getNbh() + "'," +
-	 * newUv.getCoord() + "') "; try { result =
-	 * DBAction.getStm().executeUpdate(request); } catch (SQLException ex) { if
-	 * (ex.getErrorCode() == 1062) {// la clè existe dèjè result = -2; }
-	 * System.out.println(ex.getMessage()); }
-	 * 
-	 * DBAction.DBClose(); return result; }
+	/**
+	 * Add Functions optional
+	 * @param newUv
+	 * @return success or not
 	 */
+	public static int addUv(Uv newUv) {
+		int result = -1;
+		String request = null;
 
-	/* UPDATE Functions */
+		DBAction.DBConnexion();
+		request = "INSERT INTO uv (code, nbh, coord)" + " VALUES ('" + newUv.getCode() + "'," + newUv.getNbh() + ",'"
+				+ newUv.getCoord() + "') ";
+		try {
+			result = DBAction.getStm().executeUpdate(request);
+		} catch (SQLException ex) {
+			if (ex.getErrorCode() == 1062) {
+				System.out.println(ex.getMessage());
+			}
+		}
+
+		DBAction.DBClose();
+		return result;
+	}
+
+	/**
+	 * Update NBH Functions
+	 * 
+	 * @param code   of UV
+	 * @param newNbh (new number of hours for UV)
+	 * @return success or error
+	 */
 	public static int updateNbhByCode(String code, int newNbh) {
 		int result = -1;
 		String request = null;
 		DBAction.DBConnexion();
-		request = "UPDATE uv SET nbh ='" + newNbh + "'WHERE code ='" + code + "'";
-		try {
-			result = DBAction.getStm().executeUpdate(request);
-		} catch (SQLException ex) {
-			System.out.println(ex.getMessage());
-		}
-
+		if(newNbh >0) {
+			request = "UPDATE uv SET nbh ='" + newNbh + "'WHERE code ='" + code + "'";
+			try {
+				result = DBAction.getStm().executeUpdate(request);
+			} catch (SQLException ex) {
+				System.out.println(ex.getMessage());
+			}
+		} 
+		
 		DBAction.DBClose();
 		return result;
 	}
 
+	/**
+	 * Update coord by code
+	 * 
+	 * @param code     of the uv
+	 * @param newCoord for the uv
+	 * @return success or error
+	 */
 	public static int updateCoordByCode(String code, String newCoord) {
 		int result = -1;
 		String request = null;
 		DBAction.DBConnexion();
-		request = "UPDATE uv SET coord ='" + newCoord + "'WHERE code ='" + code + "'";
-		try {
-			result = DBAction.getStm().executeUpdate(request);
-		} catch (SQLException ex) {
-			System.out.println(ex.getMessage());
+		if(newCoord!=null) {
+			request = "UPDATE uv SET coord ='" + newCoord + "'WHERE code ='" + code + "'";
+			try {
+				result = DBAction.getStm().executeUpdate(request);
+			} catch (SQLException ex) {
+				System.out.println(ex.getMessage());
+			}
 		}
+		
 
 		DBAction.DBClose();
 		return result;
 	}
-	/* DELETE Functions */
+
+	/**
+	 * Delete Uv by Code
+	 * 
+	 * @param code of Uv
+	 * @return success or error
+	 */
 
 	public static int deleteUvByCode(String code) {
 		int result = -1;
 		String request = null;
-		// 1: connexion è la BD
+		// 1: connexion a la BD
 		DBAction.DBConnexion();
 		// 2: prÃ©parer ma requpete de suppression
 		request = "DELETE FROM uv WHERE code ='" + code + "'";
--- EcoleModel2/src/com/crea/dev4/ecole/model/test/UvDaoTest.java
+++ EcoleModel2/src/com/crea/dev4/ecole/model/test/UvDaoTest.java
@@ -2,38 +2,94 @@ package com.crea.dev4.ecole.model.test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import com.crea.dev4.ecole.model.beans.Uv;
+import com.crea.dev4.ecole.model.dao.UvDao;
+
 public class UvDaoTest {
+	@Before
+	public void testAddListeUvToTest() {
+		int insert = 1;
+	
+		assertEquals(insert, UvDao.addUv(new Uv("JAVA_Grp2", 25, "TAHA")));
+		assertEquals(insert, UvDao.addUv(new Uv("JAVA_Grp3", 20, "RIDENE")));
+		assertEquals(insert, UvDao.addUv(new Uv("JAVA_Grp4", 30, "TAHA")));
+		assertEquals(insert, UvDao.addUv(new Uv("JAVA_Grp5", 60, "TAHA")));
+
+	}
+
+	@After
+	public void testDeleteListeUvToTest() {
+		int delete = 1;
+		assertEquals(delete, UvDao.deleteUvByCode("JAVA_Grp2"));
+		assertEquals(delete, UvDao.deleteUvByCode("JAVA_Grp3"));
+		assertEquals(delete, UvDao.deleteUvByCode("JAVA_Grp4"));
+		assertEquals(delete, UvDao.deleteUvByCode("JAVA_Grp5"));
+		
+	}
 
 	@Test
 	public void testGetUvByCode() {
-		fail("Not yet implemented");
+		System.out.println("Test get UV by code");
+		Uv u_ref =new Uv("JAVA_Grp2", 25, "TAHA");
+		Uv u = new Uv();
+		u = UvDao.getUvByCode("JAVA_Grp2");
+	
+		assertEquals(u_ref.getCode(), u.getCode());
+		assertEquals(u_ref.getNbh(), u.getNbh());
+		assertEquals(u_ref.getCoord(), u.getCoord());
 	}
 
 	@Test
 	public void testGetAllUvs() {
-		fail("Not yet implemented");
+		System.out.println("Test get all UV");
+		ArrayList<Uv> liste = new ArrayList<Uv>();
+		liste = UvDao.getAllUvs();
+		System.out.println(liste);
 	}
 
 	@Test
 	public void testGetUvsWithHours() {
-		fail("Not yet implemented");
+		System.out.println("Test get all UV > number of hours");
+		ArrayList<Uv> liste = new ArrayList<Uv>();
+		liste = UvDao.getUvsWithHours(50);
+		System.out.println(liste);
+		for (Uv u : liste) {
+			
+			assertEquals(60,u.getNbh());
+		}
 	}
 
 	@Test
 	public void testUpdateNbhByCode() {
-		fail("Not yet implemented");
+		int update_false = -1;
+		int update_true = 1;
+		assertEquals(update_false, UvDao.updateNbhByCode("JAVA_Grp2", -20));
+		assertEquals(update_true, UvDao.updateNbhByCode("JAVA_Grp2", 40));
 	}
 
 	@Test
 	public void testUpdateCoordByCode() {
-		fail("Not yet implemented");
+		int update_false = -1;
+		int update_true = 1;
+		assertEquals(update_false, UvDao.updateCoordByCode("JAVA_Grp2", null));
+		assertEquals(update_true, UvDao.updateCoordByCode("JAVA_Grp2", "M. Ridene"));
 	}
 
 	@Test
 	public void testDeleteUvByCode() {
-		fail("Not yet implemented");
+		int delete_true = 1;
+		int delete_false = 0;
+		
+		assertEquals(delete_true, UvDao.addUv(new Uv("JAVA_Grp55", 46, "Toto")));//insert
+		
+		assertEquals(delete_false, UvDao.deleteUvByCode("JAVA_Grp5555")); //False
+		assertEquals(delete_true, UvDao.deleteUvByCode("JAVA_Grp55"));//true
 	}
 
 }
