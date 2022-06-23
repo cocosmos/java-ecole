b79753b58c4196811bcdbc5245537d8faab6adaf
Cocosmos <mipam.guillot@crea-inseec.com>
2022-06-23 19:18:53 +0200

    Add Javadoc

--- README.md
+++
@@ -1,5 +0,0 @@
-# TP Java Interface + Model Ecole
-
-## Mipam Guillot
-## 23.06.2022
-
---
+++ java-doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.crea.dev4.ecole.inter.controleur"},{"l":"com.crea.dev4.ecole.inter.metier"}];updateSearchResults();
